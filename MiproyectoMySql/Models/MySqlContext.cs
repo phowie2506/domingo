9a382432a40721ace4c9fef1cfa36d5b66b52abd
phowie2506 <riverav.ronnie@gmail.com>
2024-11-24 17:17:49 -0500

    changeSql Fix

--- MiproyectoMySql/Models/MySqlContext.cs
+++ MiproyectoMySql/Models/MySqlContext.cs
@@ -20,7 +20,7 @@ public partial class MySqlContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=sena.mysql.database.azure.com;database=sena;user=sena;password=P4ssw0rd;sslmode=Preferred", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.39-mysql"));
+        => optionsBuilder.UseMySql("server=;database=;user=;password=;sslmode=Preferred", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.39-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
